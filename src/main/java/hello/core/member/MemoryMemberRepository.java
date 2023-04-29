9006c6e83dba57076cfeef2f2b4d97fa76276e81
devjun <devjun63@gmail.com>
2023-04-29 22:50:45 +0900

    회원 도메인 개발

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();   //동시성 이슈 때문에 concurrenthashmap를 사용하는게 맞으나 예제이므로 간단히 hashMap으로 대체
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
